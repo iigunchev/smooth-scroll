96d10f20ba0c3165209761961d8bde3d926b4652
iigunchev <iigunchev@gmail.com>
2022-04-19 17:55:12 +0200

    featured section content and styles

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,7 +1,22 @@
 import React from "react";
+
 import "./style.scss";
+import photos from "../../utils/photos";
+
 const Featured = () => {
-  return <section className="featured-section">Featured</section>;
+  const [firstUrl, secondUrl] = photos;
+  return (
+    <section className="featured-section">
+      <div className="featured-row-layout">
+        <h6>sea</h6>
+        <img src={firstUrl} alt=""/>
+      </div>
+      <div className="featured-column-layout">
+        <h6>mountain</h6>
+        <img src={secondUrl} alt=""/>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
---
+++ src/utils/photos.js
@@ -0,0 +1,7 @@
+const photos = [
+  'https://images.unsplash.com/photo-1480109866847-0b432ceb666a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  'https://images.unsplash.com/photo-1542235222-30e843cb43a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
+];
+
+
+export default photos;
