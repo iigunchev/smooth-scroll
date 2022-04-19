9ce23de21409a730f33ffcaf11122ca7c673a11e
iigunchev <iigunchev@gmail.com>
2022-04-19 18:06:06 +0200

    about content and styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,24 @@
 import React from "react";
+import SectionHeader from "../SectionHeader/SectionHeader";
 import "./style.scss";
 
 const About = () => {
-  return <section className="about-section">About</section>;
+  return (
+    <section className="about-section">
+      <SectionHeader title="about" />
+      <p id="headline">
+        We are united by our passion for the unseen, the unexplored, the unconquered, and enriching journeys. 
+      </p>
+      <p id="headline">
+        When traveling the world, we always felt that the most amazing experiences should be shared.
+      </p>
+      <p id="headline">
+        We are skiers,
+        snowboarders, hikers, climbers, bikers, sailors, surfers, guides, and we
+        love making people smile.
+      </p>
+    </section>
+  );
 };
 
 export default About;
