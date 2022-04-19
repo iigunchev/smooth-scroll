d8fa59a0eeede9d4c9ea72cd51371f75304923ef
iigunchev <iigunchev@gmail.com>
2022-04-19 17:27:01 +0200

    home and header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,16 @@ import React, { useEffect, useRef, useState } from "react";
 import "./style.scss";
 
 const Header = () => {
-  return <section className="header-container">Header</section>;
+  return (
+    <section className="header-container">
+      <ul className="header-menu">
+        <li>Intro</li>
+        <li>About</li>
+        <li>Featured</li>
+      </ul>
+      <h1 id="header-text">Go Explore</h1>
+    </section>
+  );
 };
 
 export default Header;
